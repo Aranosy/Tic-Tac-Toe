937d7db63b58ce43dabad3572b44abab2f0cceba
Artem Miroshnychenko <110293564+Aranosy@users.noreply.github.com>
2025-09-14 00:47:41 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -25,28 +25,68 @@ for (let i = 0; i < squareGrid.length; i++)
     // squareGrid[i].firstElementChild.classList.add("circle");
 }
 
-let  isPlayer = true; // If player does first move
+let isCrossFirst = true;
 let moveCount = 0; // Count moves
 let winner = false;
+let currentSymbol = 'x' // x starts
+let isClear = false;
 squares.forEach(square => {
     square.square.addEventListener('click', () => {
-        if (square.symbol === "")
+        if (isClear)
         {
-            square.square.innerHTML = '<?xml version="1.0" encoding="iso-8859-1"?> <svg class="cross" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"> <g> <g> <polygon points="512,59.076 452.922,0 256,196.922 59.076,0 0,59.076 196.922,256 0,452.922 59.076,512 256,315.076 452.922,512 512,452.922 315.076,256 "/> </g> </g> </svg>'
-            square.symbol = "x";
+            squares.forEach((element) => {
+                element.square.innerHTML = "";
+                element.symbol = "";
+            });
+            isClear = false;
+        }
+        else if (square.symbol === "")
+        {
+            square.square.innerHTML =  (currentSymbol === 'x') 
+            ? '<?xml version="1.0" encoding="iso-8859-1"?> <svg class="cross" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"> <g> <g> <polygon points="512,59.076 452.922,0 256,196.922 59.076,0 0,59.076 196.922,256 0,452.922 59.076,512 256,315.076 452.922,512 512,452.922 315.076,256 "/> </g> </g> </svg>'
+            : '<?xml version="1.0" encoding="utf-8"?> <svg class="circle" animation="popin 0.3s linear" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>';
+            square.square.children[0].style = "animation: popout 0.1s linear;";
+            square.square.children[0].addEventListener("animationend", () => {
+                    square.square.children[0].style = "animation: ;";
+            });
+            square.symbol = currentSymbol;
+            currentSymbol = (currentSymbol === 'x') ? 'o' : 'x'; // Change symbol
             moveCount++;
             if (moveCount >= 3)
             {
                 winner = checkWin();
                 if (winner !== false)
                 {
-                    console.log("We have a winner!: " + winner.symbol + " combination: " + winner.combination);
+                    isClear = true;
+                    squares.forEach((element) => {
+                        if (element.symbol !== "")
+                            element.square.children[0].style = "filter: invert(50%);" 
+                    });
+                    if (winner.symbol !== undefined)
+                    {
+                        let score = document.querySelector('#' + winner.symbol);
+                        score.textContent = parseInt(score.textContent) + 1;
+                        score.style = "animation: popout 0.2s linear;"
+                        winner.combination.forEach((element) => {
+                            squares[element].square.children[0].style = "filter: invert(100%);"     
+                            squares[element].square.children[0].style = "animation: blink 1s linear;";
+
+                        });
+                    }
+                    else 
+                    {
+                        let score = document.querySelector('#tie');
+                        score.textContent = parseInt(score.textContent) + 1;
+                        score.style = "animation: popout 0.2s linear;"
+                    }
                 }
             }
-            // aiMove();
+            
         }
     });
 });
+
+
 
 let getXnO = (cross, circle) => {
     for (let i = 0; i < squares.length; i++)
@@ -80,6 +120,7 @@ let checkWin = () => {
         }
         if (coincided === 3)
         {
+            isCrossFirst = (isCrossFirst) ? false : true;
             winner = { 
                 symbol: "x",
                 combination: element
@@ -93,13 +134,16 @@ let checkWin = () => {
     // Check for each win combination for circle
     combiations.forEach((element) => {
         let coincided = 0; // Count same values
-        for (let i = 0; i < circle.length; i ++)
+        for (let i = 0; i < element.length; i ++)
         {
-            if (circle[i] == element[i])
-                coincided++;
+            circle.forEach((o) => {
+                if (o == element[i])
+                    coincided++;
+            });
         }
         if (coincided === 3)
         {
+            isCrossFirst = (isCrossFirst) ? false : true;
             winner = { 
                 symbol: "o",
                 combination: element
@@ -107,61 +151,19 @@ let checkWin = () => {
         }
     });
 
-    if (isPlayer)
+    if (isCrossFirst)
     {
         if ((cross.length === 5) && (circle.length === 4))
+        {
+            isCrossFirst =  false;
             return "tie";
+        }
     }
-    else if ((cross.length === 4)) {
+    else if ((cross.length === 4) && (circle.length === 5)) {
+        isCrossFirst = true;
         return  "tie";
     }
 
+    isCrossFirst =  false;
     return winner;
-};
-
-
-function getRandomMove() {
-  return Math.floor(Math.random() * 9);
-}
-
-let setCircle = (square) => {
-    square.square.innerHTML = '<?xml version="1.0" encoding="utf-8"?> <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>'
-    square.symbol = "o";
-};
-
-let checkCombinations = (circle) => {
-    // Scroll thruogh all possible win combinations with our circles
-    let winnerMove = 0; 
-    combiations.forEach((element) => {
-    let coincided = 0; // Count same values
-        for (let i = 0; i < circle.length; i ++)
-        {
-            if (circle[i] == element[i])
-                coincided++;
-        }
-    });
-
-};
-
-let aiMove = () => {    
-    // Make first move
-    if (moveCount === 0)
-    {
-        if (squares[5].symbol === "")
-        {   
-            setCircle(squares[5].square);
-        }
-        else 
-            setCircle(squares[getRandomMove].square); // Center taken, just take random circle
-    }
-    else 
-    {
-        let cross = [];
-        let circle = [];
-        getXnO(cross, circle);
-        
-
-        // Check for possible win combinations with circles
-        checkCombinations(circle);
-    }
 };
